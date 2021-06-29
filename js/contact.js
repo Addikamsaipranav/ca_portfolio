254aa79ec1cbecc12322b3661f1478ffa202bab4
Sashi445 <sashidhar652@gmail.com>
2021-06-29 12:18:36 +0530

    ADDED VALIDATION FOR CONTACT US!

--- js/contact.js
+++ js/contact.js
@@ -16,18 +16,28 @@ const getDateTime = () => {
 };
 
 const contactUsFormUpload = () => {
-  const saveButton = document.querySelector("#submit");
+
+  const saveButton = document.getElementById("submitBtn");
+
+  console.log(saveButton);
 
   const firestoreRef = firebase.firestore();
 
-  saveButton.addEventListener("click", function (event) {
-    const name = document.getElementById("user_name").value;
-    const email = document.getElementById("user_email").value;
-    const phoneNumber = document.getElementById("user_phone").value;
-    const country = document.getElementById("user_country").value;
-    const subject = document.getElementById("user_subject").value;
-    const message = document.getElementById("user_message").value;
+  saveButton.addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    const name = document.getElementById("user_name").value === "" ? null : document.getElementById("user_name").value;
+    const email = document.getElementById("user_email").value === "" ? null :  document.getElementById("user_email").value;
+    const phoneNumber = document.getElementById("user_phone").value === "" ? null : document.getElementById("user_phone").value;
+    const country = document.getElementById("user_country").value === "" ? null : document.getElementById("user_country").value;
+    const subject = document.getElementById("user_subject").value === "" ? null : document.getElementById("user_subject").value;
+    const message = document.getElementById("user_message").value === "" ? null : document.getElementById("user_message").value;
     const messageLog = document.getElementById("messages");
+
+    if(name && email && phoneNumber && country && subject && message){
+      this.innerHTML = `<p class="fs-6">Loading... Please wait!!</p>`;
+
     const data = {
       name,
       email,
@@ -36,6 +46,7 @@ const contactUsFormUpload = () => {
       message,
       subject,
     };
+    
     const docRef = firestoreRef.doc(`contactus/${getDateTime()}`);
     docRef
       .set(data)
@@ -49,7 +60,7 @@ const contactUsFormUpload = () => {
       }).then(() => {
         setTimeout(() => {
           window.location.reload();
-        }, 500)
+        }, 100)
       })
       .catch(function (error) {
         console.log(`Got an error : ${error}`);
@@ -59,7 +70,17 @@ const contactUsFormUpload = () => {
                 </div>
           `;
       });
-  });
+    }else{
+      messageLog.innerHTML = `
+        <div class="alert alert-danger" role="alert">
+            All Fields are required!!
+        </div>
+      `;
+    }
+
+    
+  })
+
 };
 
 function app() {
