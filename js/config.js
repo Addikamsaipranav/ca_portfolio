9033d85726c5f9996def4f2c08c8378c9ff3a442
Sashi445 <sashidhar652@gmail.com>
2021-06-29 11:41:30 +0530

    ADDED CONFIG FILE

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBy65eQnSEcaiYZXdMkS5YZL-oojTmZuhU",
+    authDomain: "service-tracker-665ef.firebaseapp.com",
+    projectId: "service-tracker-665ef",
+    storageBucket: "service-tracker-665ef.appspot.com",
+    messagingSenderId: "489936846435",
+    appId: "1:489936846435:web:117314c7a255da467ac256",
+    measurementId: "G-4FCXYN4PH0",
+  };
